c0dd3cc6fc27ff090fac2c6145cf18addd1a5b53
Adriel Klem <adrielklem16@gmail.com>
2023-09-15 16:35:04 -0300

    add logically auth

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,10 +1,32 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { api } from "../services/api";
 
-const AuthContext = createContext({})
+export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+    const [ data, setData ] = useState({})
+    
+    async function signIn({ email, password }) {
+
+        try{
+            const response = await api.post("/sessions", { email, password })
+            const { user, token } = response.data
+
+            api.defaults.headrs.authorization = `Bearer ${token}`
+            setData({ user, token })
+
+            console.log(user, token)
+        } catch (error) {
+            if(error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert("Não foi possível entrar")
+            }
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{name: "Joao", email: "joao@email.com"}}>
+        <AuthContext.Provider value={{ signIn, user: data.user }}>
             {children}
         </AuthContext.Provider>
     )
@@ -16,4 +38,4 @@ function useAuth() {
     return context
 }
 
-export { AuthContext, AuthProvider, useAuth }
+export { AuthProvider, useAuth }
