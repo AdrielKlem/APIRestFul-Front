106387a748e379b60eef8f11d50a838fb583c791
Adriel Klem <adrielklem16@gmail.com>
2023-09-15 11:36:59 -0300

    update services SignUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
+import { api } from "../../services/api";
 
 export function SignUp() {
     const [name, setName] = useState("") 
@@ -17,7 +18,19 @@ export function SignUp() {
             return alert("Preencha todos os campos!")
         }
 
-        
+        api.post("/users", { name, email, password })
+        .then(() => {
+            alert("Usuário cadastrado com sucesso!")
+        })
+        .catch(error => {
+            if(error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert("Não foi possível cadastrar o usuário")
+            }
+        })
+
+
     }
 
     return (
