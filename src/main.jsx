e2b5ef0b1d8c054b1f884fc3f6a3c44f45b7d419
Adriel Klem <adrielklem16@gmail.com>
2023-09-15 15:51:40 -0300

    add hook auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
-import { myContext } from './myContext'
+import { ThemeProvider } from 'styled-components'
+import { AuthProvider } from './hooks/auth'
 import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <myContext.Provider value={{email: "joao@email.com"}}>
-        <Routes />
-      </myContext.Provider>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
