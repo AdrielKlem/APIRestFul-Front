df5f10b94a295ee8a5abe102ff7b732b8bdf87ff
Adriel Klem <adrielklem16@gmail.com>
2023-09-16 17:49:14 -0300

    create logically remove note tags

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -30,6 +30,9 @@ export function New() {
         setNewTag("")
     }
     
+    function handleRemoveTag(deleted) {
+        setTags(prevState => prevState.filter(tag => tag !== deleted))
+    }
 
     return(
         <Container>
@@ -70,9 +73,7 @@ export function New() {
                                 <NoteItem 
                                     key={String(index)}
                                     value={tag}
-                                    onClick={() => {
-                                        
-                                    }}
+                                    onClick={() => handleRemoveTag(tag)}
                                 />
                             ))
                         }
