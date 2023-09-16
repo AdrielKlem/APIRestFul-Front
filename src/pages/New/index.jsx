9f53e16f5667d4899bacae9ad097396359afab55
Adriel Klem <adrielklem16@gmail.com>
2023-09-16 18:52:50 -0300

    validation notes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -41,6 +41,18 @@ export function New() {
     }
 
     async function handleNewNote() {
+        if(!title) {
+            return alert("Digite o título da nota")
+        }
+
+        if(newLink) {
+            return alert("Você deixou um LINK no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe vazio")
+        }
+
+        if(newTag) {
+            return alert("Você deixou uma TAG no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe vazio")
+        }
+
         await api.post("/notes", {
             title,
             description,
