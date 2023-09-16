e2b5a841c0f59e2fa70aa57004f7646180514671
Adriel Klem <adrielklem16@gmail.com>
2023-09-16 20:17:52 -0300

    load data in details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
 import { Container, Links, Content } from "./styles"
+import { api } from "../../services/api"
 
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
@@ -7,37 +10,78 @@ import { ButtonText } from "../../components/ButtonText"
 import { Tag } from "../../components/Tag"
 
 export function Details() {
+  const [data, setData] = useState(null)
+  const params = useParams()
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate("/")
+  }
+  
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  },[])
+
   return (
     <Container>
     <Header/>
+    {
+      data && //renderiza apenas quando existir o objeto
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota" />
 
-    <main>
-      <Content>
-        <ButtonText title="Excluir Nota" />
+          <h1>
+            {data.title}
+          </h1>
+          
+          <p>
+            {data.description}
+          </p>
+        {
+          data.links && 
+          <Section title="Links úteis">
+            <Links>
+              {
+                data.links.map(link => (
+                  <li key={String(link.id)}>
+                    <a 
+                    href={link.url}
+                    target="_blank"
+                    >
+                      {link.url}  
+                    </a>
+                  </li>
+                ))
+              }
+            </Links>
+          </Section>
+        }
 
-        <h1>
-          Introdução ao ReactJS
-        </h1>
-        
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates magni minima nemo ex. Recusandae nesciunt non consequatur modi in ea quas consequuntur. Rem, laborum. Possimus alias laudantium optio porro reiciendis.
-        </p>
-
-        <Section title="Links úteis">
-          <Links>
-            <li><a href="#">https:/rocketseat.com.br</a></li>
-            <li><a href="#">https:/rocketseat.com.br</a></li>
-          </Links>
-        </Section>
-
-        <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="node" />
-        </Section>
-
-        <Button title="Voltar"/>      
-      </Content>
-    </main>
+        {
+          data.tags &&
+          <Section title="Marcadores">
+            {
+              data.tags.map(tag => (
+                <Tag 
+                key={String(tag.id)}
+                title={tag.name}/>
+              ))
+            }
+          </Section>
+        }
+          <Button 
+          title="Voltar"
+          onClick={handleBack}
+          />      
+        </Content>
+      </main>
+    }
     </Container>
   )
 }
