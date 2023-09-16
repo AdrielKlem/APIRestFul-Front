94b098e50ec3f256e402fba608e46e4078c3f6c9
Adriel Klem <adrielklem16@gmail.com>
2023-09-16 15:22:46 -0300

    add function to load data users

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
-import { Container, Form, Avatar } from "./styles"
 import { Link } from "react-router-dom"
 
+import { useAuth } from '../../hooks/auth'
+
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+
+import { Container, Form, Avatar } from "./styles"
+
 export function Profile() {
+    const { user } = useAuth()
+
+    const [name, setName] = useState(user.name)
+    const [email, setEmail] = useState(user.email)
+    const [passwordOld, setPasswordOld] = useState()
+    const [passwordNew, setPasswordNew] = useState()
+
     return (
         <Container>
             <header>
@@ -31,21 +43,27 @@ export function Profile() {
                     placeholder="Nome"
                     type="text"
                     icon={FiUser}
+                    value={name}   
+                    onChange={e => setName(e.target.value)}                 
                 />
                 <Input 
                     placeholder="Email"
                     type="text"
                     icon={FiMail}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}                 
                 />
                 <Input 
                     placeholder="Senha Atual"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPasswordOld(e.target.value)}                 
                 />
                 <Input 
                     placeholder="Nova senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPasswordNew(e.target.value)}                 
                 />
 
                 <Button title="Salvar"/>
